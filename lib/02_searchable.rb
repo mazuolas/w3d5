ebba189849c4a58409a9b08601bb92adf44eee3c
mark azuolas <markazuolas@gmail.com>
2017-04-14 13:41:58 -0700

    completed 02

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -4,9 +4,25 @@ require_relative '01_sql_object'
 module Searchable
   def where(params)
     # ...
+
+    param_str = []
+    params.each do |key, val|
+      param_str << "#{key.to_s} = ?"
+    end
+    param_str = param_str.join(" AND ")
+    results = DBConnection.execute(<<-SQL, params.values)
+    SELECT
+      *
+    FROM
+      #{self.table_name}
+    WHERE
+      #{param_str}
+    SQL
+    results.map { |result| self.send(:new, result)}
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
